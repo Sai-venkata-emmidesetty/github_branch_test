79759275857abd305df536fb85d35b1c3f0a1332
sai-venkata-emmidesetty <saivenkatev17@gmail.com>
2025-02-15 18:58:43 +0530

    added the s3_key

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "lambda" {
   source = "./modules/lambda"
   lambda_name = "dev_env_snowflake" 
   s3_bucket = "edl-mig" 
-  s3_key = "global_pay/bronze/app.zip"   
+  s3_key = "global_pay/bronze_edl_mig/app.zip"   
 }
 
 resource "null_resource" "trigger_lambda" {
@@ -13,7 +13,7 @@ EOT
   }
 
   depends_on = [module.lambda]
-  depends_on = [module.lambda]
+  
 }
 
 
